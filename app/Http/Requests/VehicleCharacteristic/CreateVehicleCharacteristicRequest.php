e75d7cae8f41a93747c764d3dea4e9e59c70a05b
fanebrahima <brahimafane06@gmail.com>
2025-12-16 22:46:58 +0000

    Update

--- app/Http/Requests/VehicleCharacteristic/CreateVehicleCharacteristicRequest.php
+++ app/Http/Requests/VehicleCharacteristic/CreateVehicleCharacteristicRequest.php
@@ -2,11 +2,10 @@
 
 namespace App\Http\Requests\VehicleCharacteristic;
 
-use App\Models\Usage;
 use App\Models\VehicleEnergy;
 use App\Models\Dealer;
 use App\Models\VehicleModel;
-use App\Models\VehicleGenre;
+use App\Models\VehicleGenreUsage;
 use Illuminate\Foundation\Http\FormRequest;
 
 class CreateVehicleCharacteristicRequest extends FormRequest
@@ -15,8 +14,7 @@ class CreateVehicleCharacteristicRequest extends FormRequest
     {
         $this->merge([
             "vehicle_model_id" => $this->vehicle_model_id ? VehicleModel::keyFromHashId($this->vehicle_model_id) : null,
-            "vehicle_genre_id" => $this->vehicle_genre_id ? VehicleGenre::keyFromHashId($this->vehicle_genre_id) : null,
-            "usage_id" => $this->usage_id ? Usage::keyFromHashId($this->usage_id) : null,
+            "vehicle_genre_usage_id" => $this->vehicle_genre_usage_id ? VehicleGenreUsage::keyFromHashId($this->vehicle_genre_usage_id) : null,
             "vehicle_energy_id" => $this->vehicle_energy_id ? VehicleEnergy::keyFromHashId($this->vehicle_energy_id) : null,
             "dealer_id" => $this->dealer_id ? Dealer::keyFromHashId($this->dealer_id) : null,
         ]);
@@ -26,8 +24,7 @@ class CreateVehicleCharacteristicRequest extends FormRequest
     {
         return [
             "vehicle_model_id" => "required|exists:vehicle_models,id",
-            "vehicle_genre_id" => "required|exists:vehicle_genres,id",
-            "usage_id" => "required|exists:usages,id",
+            "vehicle_genre_usage_id" => "required|exists:vehicle_genre_usages,id",
             "vehicle_energy_id" => "required|exists:vehicle_energies,id",
             "dealer_id" => "required|exists:dealers,id",
             "type" => "nullable|string|max:255",
@@ -44,10 +41,8 @@ class CreateVehicleCharacteristicRequest extends FormRequest
         return [
             "vehicle_model_id.required" => "Le modèle de véhicule est requis.",
             "vehicle_model_id.exists" => "Le modèle de véhicule n'existe pas.",
-            "vehicle_genre_id.required" => "Le genre de véhicule est requis.",
-            "vehicle_genre_id.exists" => "Le genre de véhicule n'existe pas.",
-            "usage_id.required" => "L'usage est requis.",
-            "usage_id.exists" => "L'usage n'existe pas.",
+            "vehicle_genre_usage_id.required" => "Le genre de véhicule est requis.",
+            "vehicle_genre_usage_id.exists" => "Le genre de véhicule n'existe pas.",
             "vehicle_energy_id.required" => "L'énergie est requise.",
             "vehicle_energy_id.exists" => "L'énergie n'existe pas.",
             "dealer_id.required" => "Le concessionnaire est requis.",
--- app/Http/Requests/VehicleCharacteristic/UpdateVehicleCharacteristicRequest.php
+++ app/Http/Requests/VehicleCharacteristic/UpdateVehicleCharacteristicRequest.php
@@ -3,11 +3,10 @@
 namespace App\Http\Requests\VehicleCharacteristic;
 
 use Illuminate\Foundation\Http\FormRequest;
-use App\Models\Usage;
 use App\Models\VehicleEnergy;
 use App\Models\Dealer;
 use App\Models\VehicleModel;
-use App\Models\VehicleGenre;
+use App\Models\VehicleGenreUsage;
 
 class UpdateVehicleCharacteristicRequest extends FormRequest
 {
@@ -15,8 +14,7 @@ class UpdateVehicleCharacteristicRequest extends FormRequest
     {
         $this->merge([
             "vehicle_model_id" => $this->vehicle_model_id ? VehicleModel::keyFromHashId($this->vehicle_model_id) : null,
-            "vehicle_genre_id" => $this->vehicle_genre_id ? VehicleGenre::keyFromHashId($this->vehicle_genre_id) : null,
-            "usage_id" => $this->usage_id ? Usage::keyFromHashId($this->usage_id) : null,
+            "vehicle_genre_usage_id" => $this->vehicle_genre_usage_id ? VehicleGenreUsage::keyFromHashId($this->vehicle_genre_usage_id) : null,
             "vehicle_energy_id" => $this->vehicle_energy_id ? VehicleEnergy::keyFromHashId($this->vehicle_energy_id) : null,
             "dealer_id" => $this->dealer_id ? Dealer::keyFromHashId($this->dealer_id) : null,
         ]);
@@ -25,8 +23,7 @@ class UpdateVehicleCharacteristicRequest extends FormRequest
     {
         return [
             "vehicle_model_id" => "required|exists:vehicle_models,id",
-            "vehicle_genre_id" => "required|exists:vehicle_genres,id",
-            "usage_id" => "required|exists:usages,id",
+            "vehicle_genre_usage_id" => "required|exists:vehicle_genre_usages,id",
             "vehicle_energy_id" => "required|exists:vehicle_energies,id",
             "dealer_id" => "required|exists:dealers,id",
             "type" => "nullable|string|max:255",
@@ -42,10 +39,8 @@ class UpdateVehicleCharacteristicRequest extends FormRequest
         return [
             "vehicle_model_id.required" => "Le modèle de véhicule est requis.",
             "vehicle_model_id.exists" => "Le modèle de véhicule n'existe pas.",
-            "vehicle_genre_id.required" => "Le genre de véhicule est requis.",
-            "vehicle_genre_id.exists" => "Le genre de véhicule n'existe pas.",
-            "usage_id.required" => "L'usage est requis.",
-            "usage_id.exists" => "L'usage n'existe pas.",
+            "vehicle_genre_usage_id.required" => "Le genre de véhicule est requis.",
+            "vehicle_genre_usage_id.exists" => "Le genre de véhicule n'existe pas.",
             "vehicle_energy_id.required" => "L'énergie est requise.",
             "vehicle_energy_id.exists" => "L'énergie n'existe pas.",
             "dealer_id.required" => "Le concessionnaire est requis.",
