294e75190e3b73fac8d9de43c1e97ba416ae9e42
fanebrahima <brahimafane06@gmail.com>
2025-12-23 10:08:32 +0000

    Update

--- routes/api/v1.dashboard.routes.php
+++ routes/api/v1.dashboard.routes.php
@@ -5,9 +5,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->name('dashboard.')->group(function () {
     Route::get('/users', [DashboardController::class, 'users'])->name('users');
-    Route::get('/assignments', [DashboardController::class, 'assignments'])->name('assignments');
-    Route::get('/insurers', [DashboardController::class, 'insurers'])->name('insurers');
-    // Route::get('/brokers', [DashboardController::class, 'brokers'])->name('brokers');
-    Route::get('/repairers', [DashboardController::class, 'repairers'])->name('repairers');
-    Route::get('/vehicles', [DashboardController::class, 'vehicles'])->name('vehicles');
+    Route::get('/calculations', [DashboardController::class, 'calculations'])->name('calculations');
+    Route::get('/vehicle-characteristics', [DashboardController::class, 'vehicleCharacteristics'])->name('vehicleCharacteristics');
 });
