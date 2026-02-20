b990a3c330ee654913afedc6dc13b5a32682aef1
fanebrahima <brahimafane06@gmail.com>
2026-02-20 00:06:32 +0000

    Update

--- database/seeders/DealerSeeder.php
+++ database/seeders/DealerSeeder.php
@@ -14,24 +14,24 @@ class DealerSeeder extends Seeder
      */
     public function run(): void
     {
-        Dealer::create([
-            'name' => 'Ivoire Auto',
-            'email' => 'ivoireauto@example.com',
-            'address' => '123 Main St, Anytown, USA',
-            'telephone' => '1234567890',
-            'status_id' => 1,
-            'created_by' => 1,
-            'updated_by' => 1,
-        ]);
+        // Dealer::create([
+        //     'name' => 'Ivoire Auto',
+        //     'email' => 'ivoireauto@example.com',
+        //     'address' => '123 Main St, Anytown, USA',
+        //     'telephone' => '1234567890',
+        //     'status_id' => 1,
+        //     'created_by' => 1,
+        //     'updated_by' => 1,
+        // ]);
 
-        Dealer::create([
-            'name' => 'Ivoire Auto 2',
-            'email' => 'dealer2@example.com',
-            'address' => '456 Main St, Anytown, USA',
-            'telephone' => '1234567880',
-            'status_id' => 1,
-            'created_by' => 1,
-            'updated_by' => 1,
-        ]);
+        // Dealer::create([
+        //     'name' => 'Ivoire Auto 2',
+        //     'email' => 'dealer2@example.com',
+        //     'address' => '456 Main St, Anytown, USA',
+        //     'telephone' => '1234567880',
+        //     'status_id' => 1,
+        //     'created_by' => 1,
+        //     'updated_by' => 1,
+        // ]);
     }
 }
