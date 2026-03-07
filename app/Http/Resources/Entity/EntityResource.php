3e41dbc3b349e5b9f1bd97b3c71c0e64fb704cf2
fanebrahima <brahimafane06@gmail.com>
2026-03-07 03:48:40 +0000

    Update

--- app/Http/Resources/Entity/EntityResource.php
+++ app/Http/Resources/Entity/EntityResource.php
@@ -14,7 +14,6 @@ class EntityResource extends JsonResource
 {
     public function toArray($request): array
     {
-        $credit = Transaction::where('entity_id', $this->id)->where('status_id', Status::where('code', StatusEnum::PERFORMED)->first()->id)->sum('quantity') - Calculation::where('entity_id', $this->id)->where('status_id', Status::where('code', StatusEnum::SUCCESS)->first()->id)->count();
         
         return [
             'id' => $this->hashId,
@@ -28,7 +27,7 @@ class EntityResource extends JsonResource
             'taxpayer_account_number' => $this->taxpayer_account_number,
             'service_description' => $this->service_description,
             'footer_description' => $this->footer_description,
-            'credit' => $credit,
+            'credit' => $this->credit(),
             'logo' => $this->logo ? url('storage/logos/'.$this->logo.'?v='.time()) : null,
             'status' => new StatusResource($this->whenLoaded('status')),
             'entity_type' => new EntityTypeResource($this->whenLoaded('entityType')),
--- app/Models/Entity.php
+++ app/Models/Entity.php
@@ -3,19 +3,20 @@
 namespace App\Models;
 
 use App\Builders\Entity\EntityBuilder;
-use App\Models\Status;
+use App\Enums\StatusEnum;
 use App\Filters\EntityFilters;
+use App\Models\Status;
+use Deligoez\LaravelModelHashId\Traits\HasHashId;
+use Deligoez\LaravelModelHashId\Traits\HasHashIdRouting;
 use Essa\APIToolKit\Filters\Filterable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Rennokki\QueryCache\Traits\QueryCacheable;
-use Deligoez\LaravelModelHashId\Traits\HasHashId;
-use Illuminate\Database\Eloquent\Relations\HasOne;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Deligoez\LaravelModelHashId\Traits\HasHashIdRouting;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Entity extends Model
 {
@@ -35,6 +36,11 @@ class Entity extends Model
      */
     
      protected $guarded = [];
+
+    public function credit(): int
+    {
+        return Transaction::where('entity_id', $this->id)->where('status_id', Status::where('code', StatusEnum::PERFORMED)->first()->id)->sum('quantity') - Calculation::where('entity_id', $this->id)->where('status_id', Status::where('code', StatusEnum::SUCCESS)->first()->id)->count();
+    }
 
     /**
      * Get the entity type this entity belongs to
