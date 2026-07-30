27f3f960a3999c98b8d457d885821b06a85a789b
Brahima FANE <brahimafane06@gmail.com>
2026-07-30 15:24:47 +0000

    Update

--- database/seeders/AppSettingSeeder.php
+++ database/seeders/AppSettingSeeder.php
@@ -58,7 +58,7 @@ class AppSettingSeeder extends Seeder
 
         AppSetting::create([
             'code' => 'credit_cost',
-            'value' => '10',
+            'value' => '5',
             'label' => 'Coût du crédit',
             'description' => 'Coût du crédit',
             'status_id' => Status::where('code', StatusEnum::ACTIVE->value)->first()->id,
